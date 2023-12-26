e8890601bd5545bc5fd44e418903f619c90cf0db
TruongAN7023 <154291936+TruongAN7023@users.noreply.github.com>
2023-12-26 19:12:03 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,26 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Khai bao bien
+		 */
+		String firstName = "Tran Viet";
+		String lastName = "Truong An";
+		int old = 21;
+		float markMath =  9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten: ");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+
+		//Khai bao hang. Hang phai viet hoa
+		final float PI = 3.14f;
+
+	}
+
+}
